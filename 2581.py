6acb7a8e44cbb59512478558dfddba631efb44c1
Cherrlyy <73522207+Cherrlyy@users.noreply.github.com>
2023-03-18 13:22:54 +0900

    solving problems

--- 2581.py
+++ 2581.py
@@ -1,33 +1,26 @@
-import sys
-sys.setrecursionlimit(10000)
+def is_prime(m, n):
+    l1 = [x for x in range(2, n+1)]
+    l2 = l1
+    for i in l2:
+        for j in l2:
+            if j % i == 0 and j != i:
+                l1.remove(j)
+    sum = 0
+    min = n
+    for i in l1:
+        if i >= m:
+            sum+=i
+            if min > i:
+                min = i
 
-def print_primenums(a, b):
-    l = []
-    for i in range(a, b+1):
-        if is_prime(i, 2):
-            l.append(i)
-    if len(l) == 0:
+    if sum == 0:
         print(-1)
     else:
-        print(sum(l))
-        print(min(l))
-# n % m 확인
-# m : n 또는 1 패스 : 2~n-1번째까지
-def is_prime(n, m):
-    # print("n, m: ",n, m)
-    if n == 1 or n == 2:
-        return False
-    else:
-        if m == n:
-            return True
-        elif n % m == 0:
-            return False
-        else:
-            return is_prime(n, m+1)
+        print(sum)
+        print(min)
 
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print_primenums(a, b)
+    m = int(input())
+    n = int(input())
 
-# 이중for문 -> 최소 O(nlogn)의 시간복잡도로 어떻게 줄이지?
+    is_prime(m, n)
