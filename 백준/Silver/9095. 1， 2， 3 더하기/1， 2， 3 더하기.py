ca5d7bf267c23ff51453abec42ac210be4a9c59d
ChaeHee Lee <ind07229@naver.com>
2024-08-05 00:08:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,13 +1,10 @@
 t = int(input())
 for i in range(t):
     n = int(input())
-    l = [0 for _ in range(n+1)]
+    l = [0 for _ in range(12)]
     l[1] = 1
-    for j in range(2, n+1):
-        if j == 2:
-            l[2] = l[1] + 1
-        elif j == 3:
-            l[3] = l[1] + l[2] + 1
-        else:
-            l[j] = l[j-3] + l[j-2] + l[j-1]
+    l[2] = l[1] + 1
+    l[3] = l[1] + l[2] + 1
+    for j in range(4, n+1):
+        l[j] = l[j-3] + l[j-2] + l[j-1]
     print(l[n])
