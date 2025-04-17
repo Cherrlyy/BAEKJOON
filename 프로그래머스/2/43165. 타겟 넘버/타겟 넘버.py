e55af7d29bdccc2ca8ffa03e239a0c201cff425a
ChaeHee Lee <ind07229@naver.com>
2025-04-17 21:07:56 +0900

    [level 2] Title: 타겟 넘버, Time: 304.57 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,3 @@
-from itertools import combinations
-
 def solution(numbers, target):
-    n = (sum(numbers) - target)//2
-    l = []
-    for i in range(1, len(numbers)):
-        l.extend(list(filter(lambda x: sum(x) == n, combinations(numbers, i))))
-
-    answer = len(l)
+    answer = 0
     return answer
