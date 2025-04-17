8def1aaabf0d59745b18beffb4d61fc88a208db2
ChaeHee Lee <ind07229@naver.com>
2025-04-17 21:20:17 +0900

    [level 2] Title: 타겟 넘버, Time: 399.34 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,7 @@
 def solution(numbers, target):
-    answer = 0
-    return answer
+    if target == 0 and not numbers: 
+        return 1
+    elif not numbers:
+        return 0
+    return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+    
