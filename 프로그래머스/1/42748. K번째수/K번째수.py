65825dffd2d158d57e3d06a4de1535f07608c863
ChaeHee Lee <ind07229@naver.com>
2025-04-17 16:38:36 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,20 +1,8 @@
-def return_answer(l, k):
-    new = [l[0]]
-    for i in range(1, len(l)):
-        for e in range(len(new)):
-            if l[i] <= new[e]:
-                new.insert(e, l[i])
-                break
-        else:
-            new.append(l[i])
-    return new[k-1]
-                
-    
-
 def solution(array, commands):
     answer = []
     for c in commands:
         a, b, k = c[0], c[1], c[2]
-        num = return_answer(array[a-1:b], k)
+        num = sorted(array[a-1:b])[k-1]
         answer.append(num)
+
     return answer
